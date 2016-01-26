06a5c7755d9654deb6cfe883ea86aeaa95a96efa
Jimmi Corbishley <jimmi@wipster.io>
2016-01-27 08:40:42 +1300

    add commenting

--- jsx/ppro.jsx
+++ jsx/ppro.jsx
@@ -175,6 +175,7 @@ function createSequenceMarkers(inMarkers) {
       var newMarker = sequenceMarkers.createMarker(marker.start);
       newMarker.name = marker.name;
       newMarker.comments = marker.comments.replace(/<br>/g, '\n');
+      $.writeLn('Marker end: ', marker.end)
       newMarker.end = marker.end;
     }
   }
